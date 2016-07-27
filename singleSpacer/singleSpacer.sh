f6c8513b9e249e18b21fe60556f2c30d3cd59c33
Justin C. Bagley <justincbagley@users.noreply.github.com>
2016-07-26 23:54:21 -0300

    Update and rename singleSpacer.sh to singleSpacer/singleSpacer.sh

--- singleSpacer/singleSpacer.sh
+++ singleSpacer/singleSpacer.sh
@@ -17,12 +17,12 @@ echo "
 #
 #
 #
-for i in ./*_spaced_toy.txt			## Identify and loop through double-spaced input text (.txt) files in current directory.
+for i in ./*_spaced_toy.txt						## Identify and loop through double-spaced input text (.txt) files in current directory.
 	do 
 	echo $i
-	sed '/^$/d' ${i} > ${i}.tmp			## Use sed to convert files from double to single spaced.
-	mv ${i}.tmp ${i}_singleSpaced.txt	## Rename output. 
-		for j in *.txt_singleSpaced.txt					## Nested loop to cleanup filenames.
+	sed '/^$/d' ${i} > ${i}.tmp					## Use sed to convert files from double to single spaced.
+	mv ${i}.tmp ${i}_singleSpaced.txt				## Rename output. 
+		for j in *.txt_singleSpaced.txt				## Nested loop to cleanup filenames.
 			do
 			mv $j ${j/.txt_singleSpaced.txt/.out.txt}	## Rename final output file to be kept in working directory.
 		done
