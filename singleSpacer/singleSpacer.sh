ceb34930bb9e232701b38faa4e7951c594d57da4
Justin C. Bagley <justincbagley@users.noreply.github.com>
2017-04-07 23:55:24 -0400

    Update singleSpacer.sh

--- singleSpacer/singleSpacer.sh
+++ singleSpacer/singleSpacer.sh
@@ -2,7 +2,7 @@
 
 ##########################################################################################
 #                             singleSpacer v1.0, July 2016                               #
-#   SHELL SCRIPT FOR CONVERTING FILES FROM DOUBLE/MULTIPLE SPACED TO SINGLE SPACED       #
+#   SHELL SCRIPT CONVERTING TEXT FILES FROM DOUBLE/MULTIPLE SPACED TO SINGLE SPACED      #
 #   Copyright (c)2016 Justin C. Bagley, Universidade de Brasília, Brasília, DF, Brazil.  #
 #   See the README and license files on GitHub (http://github.com/justincbagley) for     #
 #   further information. Last update: July 8, 2016. For questions, please email          #
@@ -15,19 +15,18 @@ echo "
 ##########################################################################################
 "
 
-for i in ./*_spaced_toy.txt						## Identify and loop through double-spaced input text (.txt) files in current directory.
-	do 
-	echo $i
-	sed '/^$/d' ${i} > ${i}.tmp					## Use sed to convert files from double to single spaced.
-	mv ${i}.tmp ${i}_singleSpaced.txt				## Rename output. 
-		for j in *.txt_singleSpaced.txt				## Nested loop to cleanup filenames.
-			do
+(
+for i in $(find . -name "*.txt" -type f); do				## Identify and loop through double-spaced input text (.txt) files in current directory.
+	echo "$i"
+	sed '/^$/d' "${i}" > "${i}".tmp					## Use sed to convert files from double to single spaced.
+	mv "${i}".tmp "${i}"_singleSpaced.txt				## Rename output. 
+		for j in *.txt_singleSpaced.txt; do			## Nested loop to cleanup filenames.
 			mv $j ${j/.txt_singleSpaced.txt/.out.txt}	## Rename final output file to be kept in working directory.
 		done
 done
+)
 
-echo "
-Done converting file to single spacing. Bye.
+echo "Done converting text file(s) in current working dir from double to single spacing. Bye.
 "
 #
 #
