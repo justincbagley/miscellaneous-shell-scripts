7ce8eef1c84a1f9eadd7f7f0d18dacc1ac26232a
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-08-06 14:42:27 -0400

    Update lengthHistFun.sh

--- lengthHistFun.sh
+++ lengthHistFun.sh
@@ -3,10 +3,10 @@
 ## Inspired by code at URL: https://stackoverflow.com/questions/16750911/count-line-lengths-in-file-using-command-line-tools
 ## USAGE: lengthHistFun <inputFile>
 ## 
-## Outputs single file named 'output.txt' containing histogram values for file line lengths.
+## Outputs single file named 'length_hist_out.txt' containing histogram values for file line lengths.
 
 lengthHistFun () {
-printf "%s %s\n" $(for line in $(cat "$1"); do printf $line | wc -c; done | sort -n | uniq -c | sed -E "s/([0-9]+)[^0-9]+([0-9]+)/\2 \1/") > lengths_out.txt
+printf "%s %s\n" $(for line in $(cat "$1"); do printf $line | wc -c; done | sort -n | uniq -c | sed -E "s/([0-9]+)[^0-9]+([0-9]+)/\2 \1/") > length_hist_out.txt
 }
 
 lengthHistFun
