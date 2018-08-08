32cb0b10c846d40e48651f726eedb88e72f755c2
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-08-07 23:48:10 -0400

    Update lengthFun.sh

--- lengthFun.sh
+++ lengthFun.sh
@@ -5,6 +5,23 @@
 ## 
 ## Outputs single 'lengths_out.txt' file containing lengths of each file line.
 
+######################################## START ###########################################
+echo "INFO      | $(date) | STEP #1: SETUP. "
+##--Make other important functions/variables:
+	CR=$(printf '\r'); 
+	calc () { 
+		bc -l <<< "$@" 
+}
+
+
+## Input file is "$1" and must be FASTA format, either sequential or interleaved with a 
+## hard wrap of some length on each line of each sequence.
+
+file="$1"
+
+sed -i '' 's/^/\>/g' "$file";
+sed -i '' 's/\n\>/\>/g' "$file";
+
 lengthFun () {
   for line in $(cat "$1"); do 
     printf $line | wc -c >> lengths_out.txt; 
